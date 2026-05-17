1ff01ee86ef64776e3a7e2d22d48ebdfa1e6c7dc
Ankithagowdasr <ankithagowda621@gmail.com>
2026-05-17 01:06:57 +0000

    added s3 upload and cocument portal

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import pymysql
+import boto3
 
 app = Flask(__name__)
 
@@ -20,6 +21,8 @@ CREATE TABLE IF NOT EXISTS posts (
 """)
 
 db.commit()
+S3_BUCKET="corp-portal-documents-ananya"
+s3=boto3.client('s3')
 @app.route('/')
 def home():
     cursor.execute("SELECT * FROM posts")
@@ -41,6 +44,43 @@ def create_post():
         return redirect('/')
 
     return render_template('create_post.html')
+@app.route('/upload-page')
+def upload_page():
+    return render_template('upload.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+
+    file = request.files['file']
+
+    if file:
+        s3.upload_fileobj(file, S3_BUCKET, file.filename)
+
+    return redirect('/documents')
+
+
+@app.route('/documents')
+def documents():
+
+    response = s3.list_objects_v2(Bucket=S3_BUCKET)
+
+    files = []
+
+    if 'Contents' in response:
+
+        for obj in response['Contents']:
+
+            filename = obj['Key']
+
+            url = f"https://{S3_BUCKET}.s3.amazonaws.com/{filename}"
+
+            files.append({
+                'name': filename,
+                'url': url
+            })
+
+    return render_template('documents.html', files=files)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
